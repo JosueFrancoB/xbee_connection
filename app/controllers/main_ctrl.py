119e799f45e937fbb2f3221fa4ac8dda5b7946d5
JosueFrancoB <gerardo@cbin.mx>
2021-08-09 17:54:25 -0500

    redis connect changes

--- app/controllers/main_ctrl.py
+++ app/controllers/main_ctrl.py
@@ -9,38 +9,44 @@ def validate_open_device(device):
     except:
         print("Please restart your RaspBerry for apply app configurations")
 
+
 def send_frame_xbee(device, remote_device, data):
     data = str(data.data.decode('utf8'))
     print(data)
     if data != None:
         device.send_data(remote_device, data)
 
+
 def receive_frame_xbee(device, remote_device):
     while True:
         data = None
         while data == None:
             data = device.read_data_from(remote_device)
-        send_frame_xbee(device, remote_device, data)
-        # send_to_redis()
-    # device.close()
         
+        send_to_redis(data)
+        # print(data.data.decode('utf8'))
+    # device.close()
+
+
 def send_to_redis(data):
     publisher = redis.Redis(host = 'localhost', port = 6379)
-    message=""
+    message= data.data.decode('utf8')
     channel = "test"
     while message!="exit":
-        message = input("")
+        # message = input("")
         send_message = "Python : " + message
         publisher.publish(channel, send_message)
 
-def receive_from_redis():
-    subscriber = redis.Redis(host = 'localhost', port = 6379)
-    channel = 'test'
-    p = subscriber.pubsub()
-    p.subscribe(channel)
+
+def receive_from_redis(device, remote_device):
     while True:
-        message = p.get_message()
-        if message and not message['data'] == 1:
-            message = message['data'].decode('utf-8')
-            print(message)
-        send_frame_xbee()
+        subscriber = redis.Redis(host = 'localhost', port = 6379)
+        channel = 'test'
+        p = subscriber.pubsub()
+        p.subscribe(channel)
+        message = None
+        while message == None:
+            message = p.get_message()
+            if message and not message['data'] == 1:
+                message = message['data'].decode('utf-8')
+                send_frame_xbee(device, remote_device, message)
