5df71bce9bff678328f2eabe34749b7941c40bbc
JosueFrancoB <gerardo@cbin.mx>
2021-08-09 12:06:12 -0500

    comunicate xbees with threads

--- app/app.py
+++ app/app.py
@@ -1,9 +1,17 @@
 import os
-from digi.xbee.devices import XBeeDevice
-from models.devices import RemoteDevice
+import threading
+from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
+from controllers.main_ctrl import *
 
-os.system('cp /app/files_to_host/cmdline.txt /app/config_host')
-os.system('cp /app/files_to_host/config.txt /app/config_host')
+# os.system('cp /app/files_to_host/cmdline.txt /app/config_host')
+# os.system('cp /app/files_to_host/config.txt /app/config_host')
 
-device = XBeeDevice("/dev/ttyS0", 9600)
-device = RemoteDevice(device, "0013A20040BA105F")
+device = XBeeDevice("COM3", 9600)
+remote = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A20040BA105F"))
+
+validate_open_device(device)
+
+while True:
+    threading.Thread(target=receive_frame_xbee, args=(device, remote)).start()
+
+# send_frame_xbee(device, remote, "Hola")
