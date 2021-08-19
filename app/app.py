c6e1156ce531837828ca90831e728df98b4d6dbb
David Ruiz Avalos <davidelchido8@gmail.com>
2021-08-19 11:30:58 -0500

    Flujo funcionando

--- app/app.py
+++ app/app.py
@@ -2,12 +2,13 @@ import os
 import threading
 from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
 from controllers.main_ctrl import *
+import multiprocessing
 
 # Configurations in RaspBerry with ubuntu
 # os.system('cp /app/files_to_host/cmdline.txt /app/config_host')
 # os.system('cp /app/files_to_host/config.txt /app/config_host')
 
-device = XBeeDevice("COM3", 9600)
+device = XBeeDevice("/dev/ttyS0", 9600)
 remote1 = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A20040BA105F"))
 
 validate_open_device(device)
@@ -16,11 +17,11 @@ xbee_thread = threading.Thread(target=receive_frame_xbee, args=(device, remote1,
 redis_thread = threading.Thread(target=receive_from_redis, args=(device, remote1,))
 
 if __name__ == '__main__':
-    # xbee_thread.run()
+    print("Xbee Receiver starting")    
     xbee_thread.start()
     redis_thread.start()
     xbee_thread.join()
-
+    redis_thread.join()
 
 
 
--- app/controllers/main_ctrl.py
+++ app/controllers/main_ctrl.py
@@ -1,6 +1,18 @@
 from models.devices import RemoteDevice
 from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
-import redis 
+import redis
+from gpiozero.pins.native import NativeFactory
+from gpiozero import LED
+import gpiozero
+import os
+from time import sleep
+factory = NativeFactory()
+
+led1 = LED(22,pin_factory=factory)
+led2 = LED(27,pin_factory=factory)
+led3 = LED(17,pin_factory=factory)
+relay_pin = 16
+relay = gpiozero.OutputDevice(relay_pin, active_high=True, initial_value=False, pin_factory=factory)
 
 def validate_open_device(device):
     try:
@@ -12,7 +24,6 @@ def validate_open_device(device):
 
 def send_frame_xbee(device, remote_device, data):
     data = str(data.data.decode('utf8'))
-    print(data)
     if data != None:
         device.send_data(remote_device, data)
 
@@ -20,33 +31,39 @@ def send_frame_xbee(device, remote_device, data):
 def receive_frame_xbee(device, remote_device):
     while True:
         data = None
-        while data == None:
-            data = device.read_data_from(remote_device)
-        
-        send_to_redis(data)
-        # print(data.data.decode('utf8'))
-    # device.close()
-
+        data = device.read_data_from(remote_device)
+        if data != None:
+            print(data)
+            led3.on()
+            print("Reciving frame in xbee")
+            send_to_redis(data)
 
 def send_to_redis(data):
-    publisher = redis.Redis(host = 'localhost', port = 6379)
+    publisher = redis.Redis(host = 'redis', port = 6379)
     message= data.data.decode('utf8')
-    channel = "test"
-    while message!="exit":
-        # message = input("")
-        send_message = "Python : " + message
-        publisher.publish(channel, send_message)
+    channel = "control"
+    send_message = message
+    publisher.publish(channel, send_message)
+    led2.on()
+    print("Send msg to redis -xbee")
 
 
 def receive_from_redis(device, remote_device):
+    subscriber = redis.Redis(host = 'redis', port = 6379)
+    channel = "test"
+    p = subscriber.pubsub()
+    p.subscribe(channel)
+
+
     while True:
-        subscriber = redis.Redis(host = 'localhost', port = 6379)
-        channel = 'test'
-        p = subscriber.pubsub()
-        p.subscribe(channel)
-        message = None
-        while message == None:
-            message = p.get_message()
-            if message and not message['data'] == 1:
-                message = message['data'].decode('utf-8')
-                send_frame_xbee(device, remote_device, message)
+        message = p.get_message()
+        if message != None:
+            print("Recieving redis response in xbee"+ str(message))
+            led1.on()
+            relay.on()
+            sleep(3)
+            led3.off()
+            led2.off()
+            led1.off()
+            relay.off()
+            #send_frame_xbee(device, remote_device, message)
