948cfa6013592496d9e3ff427d0bdb321ead18cb
JosueFrancoB <88593715+JosueFrancoB@users.noreply.github.com>
2021-08-30 11:58:02 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,19 +2,18 @@ import os
 import threading
 from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
 from controllers.main_ctrl import *
-import multiprocessing
 
 # Configurations in RaspBerry with ubuntu
 # os.system('cp /app/files_to_host/cmdline.txt /app/config_host')
 # os.system('cp /app/files_to_host/config.txt /app/config_host')
 
 device = XBeeDevice("/dev/ttyS0", 9600)
-remote1 = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A20040BA105F"))
+#remote1 = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string("0013A20040BA105F"))
 
 validate_open_device(device)
 # device.send_data(remote1, 'spamm')
-xbee_thread = threading.Thread(target=receive_frame_xbee, args=(device, remote1,))
-redis_thread = threading.Thread(target=receive_from_redis, args=(device, remote1,))
+xbee_thread = threading.Thread(target=receive_frame_xbee, args=(device,))
+redis_thread = threading.Thread(target=receive_from_redis, args=(device,))
 
 if __name__ == '__main__':
     print("Xbee Receiver starting")    
