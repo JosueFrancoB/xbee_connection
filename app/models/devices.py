66088225ec10b0601b71e8f95dc9d1cfd4d8c087
JosueFrancoB <88593715+JosueFrancoB@users.noreply.github.com>
2021-09-15 10:14:16 -0500

    Delete app/models directory

--- app/models/devices.py
+++
@@ -1,7 +0,0 @@
-from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
-#/dev/ttyS0
-#0013A20040BA105F
-
-class RemoteDevice: 
-    def __init__(self, device, adress_remote):
-        self.remote_device = RemoteXBeeDevice(device, XBee64BitAddress.from_hex_string(adress_remote))
